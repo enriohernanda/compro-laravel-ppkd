0dd71c22d2027961e218371a82018990d067fc45
enriohernanda <enriohernanda@gmail.com>
2025-11-11 15:10:41 +0700

    update sidebar

--- resources/views/admin/inc/sidebar.blade.php
+++ resources/views/admin/inc/sidebar.blade.php
@@ -89,8 +89,8 @@
                   </a>
                 </li>
                 <li class="menu-item">
-                  <a href="layouts-without-navbar.html" class="menu-link">
-                    <div data-i18n="Without navbar">Without navbar</div>
+                  <a href="{{ route('aboutadmin.index') }}" class="menu-link">
+                    <div data-i18n="Without navbar">About Menu</div>
                   </a>
                 </li>
                 <li class="menu-item">
